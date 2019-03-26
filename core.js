3a4b291b55340b28e85064faadd55d2f959a78fa
Dennis Schepers <dennis.schepers@jstack.eu>
2019-03-26 10:11:25 +0100

    Commit this

---
+++ core.js
@@ -0,0 +1,11 @@
+function insertAfter(el, referenceNode) {
+    referenceNode.parentNode.insertBefore(el, referenceNode.nextSibling);
+}
+
+// example
+var newEl = document.createElement('div');
+newEl.innerHTML = '<p>Hello World!</p>';
+
+var ref = document.querySelector('div.ddb-campaigns-detail-body');
+
+insertAfter(newEl, ref);
