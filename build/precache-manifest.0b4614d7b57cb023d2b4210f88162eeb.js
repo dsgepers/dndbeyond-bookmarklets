60ca80b3779991642e4ced3faf3c4cf7195a88df
Dennis Schepers <dennis.schepers@jstack.eu>
2019-03-26 10:00:10 +0100

    Commit this

---
+++ build/precache-manifest.0b4614d7b57cb023d2b4210f88162eeb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4f579197b32c18e84e92",
+    "url": "/static/css/main.00db4ec9.chunk.css"
+  },
+  {
+    "revision": "4f579197b32c18e84e92",
+    "url": "/static/js/main.77d65c95.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0571b69cddc311e66ec0",
+    "url": "/static/js/2.b934461c.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c94c61a84c4e14143ef95f534c55d5d7",
+    "url": "/index.html"
+  }
+];
