5c6d9bf85d37741819fc8fe4f4a5f808c472ec16
Dennis Schepers <dennis.schepers@jstack.eu>
2019-03-26 10:19:12 +0100

    Commit this

--- core3.js
+++ core3.js
@@ -4,8 +4,11 @@ function insertAfter(el, referenceNode) {
 
 // example
 var newEl = document.createElement('div');
-newEl.id = "dndbeyondbookmarklet";
+newEl.id = "bookmarklet-root";
 
 var ref = document.querySelector('div.ddb-campaigns-detail-header');
 
 insertAfter(newEl, ref);
+
+
+document.body.appendChild(document.createElement('script')).src='https://cdn.jsdelivr.net/gh/dsgepers/dndbeyond-bookmarklets/build/static/js/main.77d65c95.chunk.js?' + Math.random();
